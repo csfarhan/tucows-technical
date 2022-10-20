4a07d334b5e60c53106e2b65ef61ce7248df9407
Farhan Rahman <89839649+csfarhan@users.noreply.github.com>
2022-10-20 02:21:14 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use('/data', require("./routes/userRoutes"));
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
